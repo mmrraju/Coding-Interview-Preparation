cff1a61a91dd1c833e9f63c45103b4576e6fd980
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2021-10-03 22:55:21 +0600

    Update 04_LongestSubstring.py

--- Arrays and Strings/04_LongestSubstring.py
+++ Arrays and Strings/04_LongestSubstring.py
@@ -21,3 +21,31 @@ input_string = input()
 obj = Solution()
 
 print(obj.lengthOfLongestSubString(input_string))
+
+
+
+``` Using hash table  ```
+class Solution:
+    def lengthOfLongestSubString(self, s):
+        start = 0
+        dic = {}
+        max_len = 0
+        for i in range(0, len(s)):
+            if s[i] in dic.keys():
+                prev_index = dic[s[i]]+1 
+                for k in range(start, prev_index):
+                    del dic[s[k]]
+                dic[s[i]] = i
+                start = prev_index
+            else:
+                dic[s[i]] = i 
+            max_len = max((i-start)+1, max_len)
+            
+        return max_len
+        
+input_string = input()
+
+obj = Solution()
+
+print(obj.lengthOfLongestSubString(input_string))
+
